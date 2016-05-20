9e91b824d329c137920612b962e7b2bf2dd3c68e
Jon Mckenzies <jon@brandlab.co.uk>
2016-05-20 14:15:08 -0500

    ngCordova was not Injected in correctly in last commit now is

--- conf/www/js/app.js
+++ conf/www/js/app.js
@@ -4,14 +4,14 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion','ngCordova', ['ionic', 'conFusion.controllers', 'conFusion.services'])
+angular.module('conFusion', ['ionic','ngCordova', 'conFusion.controllers', 'conFusion.services'])
 
 .run(function($ionicPlatform, $rootScope, $ionicLoading,$cordovaSplashscreen, $timeout) {
   $ionicPlatform.ready(function() {
     // added after ngCordova is Ij  
         $timeout(function(){
                 $cordovaSplashscreen.hide();
-      },20000);
+      },15000);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
