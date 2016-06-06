ac8442e6f01163909c3f603fbdc6d4b0e21e7140
Jon Mckenzies <jon@brandlab.co.uk>
2016-06-06 14:48:26 -0500

    adding solve-2 and also adding rectangule-2 this now uses a callback

---
+++ nodeExamples/rectangle-2.js
@@ -0,0 +1,17 @@
+module.exports = function (x,y,callback){
+    try{
+        if(x<0 || y<0){
+            throw new Error ("not good "+ x +"need to be biger"+" or Y need tod to be bigger "+ y);
+        }
+        else {
+            callback(null,{
+                perimeter: function(){return (2*(x+y));},
+                area: function(){return (x*y);}
+            });
+        }//close to else
+            
+    }
+    catch (error){callback(Error,null);}    
+    
+}
+
