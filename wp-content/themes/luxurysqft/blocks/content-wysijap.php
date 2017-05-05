54b6549c04b1b7c87d8866bdfa3d32b1eae9ec22
Jon Mckenzies <jon@brandlab.co.uk>
2017-05-05 13:55:48 -0500

    stating again

--- wp-content/themes/luxurysqft/blocks/content-wysijap.php
+++
@@ -1,9 +0,0 @@
-<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-	<div class="title">
-	    <?php the_title( '<h1>', '</h1>' ); ?>		
-	</div>
-	<div class="content">
-		<?php the_post_thumbnail( 'full' ); ?>
-		<?php the_content(); ?>
-	</div>
-</div>
