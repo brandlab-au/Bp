017525e2d7b130d19d98ac2810c832f6048133b5
fsoria1 <fsoria@corprancagua.com>
2017-05-12 15:29:43 -0500

    Added db lookup

--- messenger/src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -1,17 +1,17 @@
 package org.koushik.javabrains.messenger.resources;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
 
 @Path("/messages")
 public class MessageResource {
-
-	@GET 
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages(){
-		return " hello from class MessageR";
-	}
 	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessages() {
+		return "hello world and raj";
+	}
+
 }
